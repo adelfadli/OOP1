8d9bf61305f8c9c4bbdb64d2e7e50da083ba5e36
adel fadli <adelfadly02@gmail.com>
2016-04-12 00:08:39 +0700

    Added files via upload

---
+++ LatihanOOP5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihanoop5;
+
+/**
+ *
+ * @author HPPAVILION
+ */
+public class LatihanOOP5 {
+
+    public static void main(String[] args) {
+      Scanner input =new Scanner (System.in);
+   String Jumlah ;
+   int Pil ;
+   Char Bil ;
+   
+
+    
+    }
+    
+}
