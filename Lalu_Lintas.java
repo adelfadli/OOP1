92f316919a80d5dc5fd6bce16f17c5596e8516b1
adel fadli <adelfadly02@gmail.com>
2016-04-12 00:18:43 +0700

    Added files via upload

---
+++ Lalu_Lintas.java
@@ -0,0 +1,390 @@
+
+package lalu_lintas;
+
+
+public class Lalu_Lintas {
+
+    
+    public static void main(String[] args) {
+     System.out.println
+("1.  Fungsi marka jalan adalah?\n" +
+"\n" +
+"    a. Untuk memberi batas jalan agar jalan terlihat jelas oleh pemakai jalan yang sedang \n" +
+"    berlalu lintas di jalan\n" +
+"    b. Untuk menambah dan mengurangi kecepatan pemakai jalan yang berlalu lintas \n" +
+"    di jalan\n" +
+"    c Untuk mengatur lalu lintas atau memperingatkan atau menuntun pemakai jalan          dalam berlalulintas\n" +
+"    d. Alat pemberi isyarat lalu lintas\n" +
+"    e. Polisi lalu lintas\n" +
+"\n" +
+"2. Yang bukan merupakan marka lambang adalah?\n" +
+"   a. Segitiga\n" +
+"   b. Gambar\n" +
+"   c. Panas\n" +
+"   d.dingin\n" +
+"   e. hijau\n" +
+"\n" +
+"3.  Rambu dengan warna dasar kuning dengan lambang atau tulisan berwarna \n" +
+"     hitam merupakan?\n" +
+"     a. Rambu petunjuk\n" +
+"     b. Rambu peringatan\n" +
+"     c. Rambu perintah\n" +
+"     d.Rambu hutan\n" +
+"     e.Rambu  merah\n" +
+"\n" +
+"4.  Garis ganda yang terdiri dari garis utuh dan garis putus-putus termasuk\n" +
+"     a. Marka membujur\n" +
+"     b. Marka melintang\n" +
+"     c. Marka serong\n" +
+"     d.Marka kiri\n" +
+"     e.Marka kanan\n" +
+"     \n" +
+"5.   Dalam suatu persimpangan jalan terdapat alat pemberi isyarat lalu lintas, polisi lalu \n" +
+"     lintas dan polisi pamong praja. Isyarat lalu lintas yang manakah yang harus diikuti \n" +
+"     oleh para pemakai jalan?\n" +
+"     a. Alat pemberi isyarat lalu lintas\n" +
+"     b. Polisi lalu lintas\n" +
+"     c. Polisi pamong praja   \n" +
+"     d.Polisi tidur\n" +
+"     e.Polisi makan\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"               6.  Anda berjalan dengan kecepatan kurang lebih 30 km per jam mendekati \n" +
+"     persimpangan yang diatur oleh lampu lalu lintas. Ketika lampu berubah dari \n" +
+"     warna hijau ke kuning,  apa yang anda lakukan?\n" +
+"    a. Berhenti\n" +
+"    b. Jalan terus\n" +
+"    c. Bersiap-siap berhenti karena belum melewati garis berhenti\n" +
+"    d.Mundur terus\n" +
+"                   e.Maju mundur\n" +
+"\n" +
+"7.   Apabila anda ingin berpindah jalur dengan aman, maka anda harus?\n" +
+"    a. Memberikan isyarat secara jelas dan tepat waktunya dengan menggunakan \n" +
+"       petunjuk arah\n" +
+"    b. Yakin bahwa tidak membahayakan pemakai jalan lain\n" +
+"    c. Kedua jawaban diatas benar\n" +
+"   \n" +
+"8.   Pengemudi diharuskan memberikan isyarat dengan petunjuk arah yang \n" +
+"     berkedip pada waktu?\n" +
+"    a. Akan berjalan atau akan merubah arah ke kanan\n" +
+"    b. Akan berjalan atau akan berhenti\n" +
+"    c. Akan merubah arah ke kiri atau ke kanan\n" +
+"    Jawaban : C\n" +
+"\n" +
+"9.   Teknik mengemudikan sepeda motor yang baik pada saat gerakan membelok \n" +
+"     adalah?\n" +
+"    a. Menambah kecepatan pada jarak pendek sebelum mencapai tikungan\n" +
+"    b. Memiringkan sepeda motor kearah pusat tikungan dan tetap dalam posisi tegak\n" +
+"    c. Memiringkan sepeda motor dan pengemudi kearah pusat tikungan yang sesuai \n" +
+"       dengan kecepatan dan ketajaman tikungan\n" +
+"    Jawaban : C\n" +
+"\n" +
+"10.  Apa kegunaan bahu jalan?\n" +
+"    a. Untuk pejalan kaki\n" +
+"    b. Untuk berhenti dan parkir\n" +
+"    c. Untuk berhenti dalam keadaan darurat\n" +
+"    Jawaban : B\n" +
+"\n" +
+"11.  Apa kegunaan helm?\n" +
+"   a. Untuk melindungi pandangan pengendara, melindungi pengendara dari panas dan \n" +
+"       hujan\n" +
+"   b. Untuk melindungi kepala dari benturan atau gesekan yang mengakibatkan luka di \n" +
+"       kepala\n" +
+"   c. Untuk menambah penampilan pengendara dan merupakan kelengkapan bagi \n" +
+"       sepeda motor\n" +
+"   Jawaban : B\n" +
+"\n" +
+"13.  Kendaraan yang digerakkan oleh peralatan teknik yang berada pada \n" +
+"     kendaraan itu disebut?\n" +
+"    a. Sepeda motor\n" +
+"    b. Kendaraan bermotor\n" +
+"    c. Motor besar\n" +
+"    Jawaban : B\n" +
+"\n" +
+"14.  Surat izin mengemudi golongan C digunakan untuk?\n" +
+"    a. Mengemudikan sepeda motor yang dirancang mampu mencapai kecepatan \n" +
+"        lebih dari 40 km per jam\n" +
+"    b. Mengemudikan sepeda motor yang dirancang mampu mencapai kecepatan \n" +
+"        tidak lebih dari 40 km per jam\n" +
+"    c. Pilihan a dan b salah\n" +
+"    Jawaban : A\n" +
+"\n" +
+"15.  Menghadapi lampu kuning yang berkedip-kedip, maka tindakan yang benar adalah?\n" +
+"     a. Berjalan terus, kendaraan lain yang harus hati-hati\n" +
+"     b. Setidak tidaknya berhenti dan beri jalan\n" +
+"     c. Mendekati dengan hati-hati, mengurangi kecepatan jika perlu berhenti \n" +
+"         dan beri jalan\n" +
+"     Jawaban : C\n" +
+"\n" +
+"16.  Dilarang mendahului kendaraan lain yang sedang berjalan walaupun tidak \n" +
+"     ada rambu-rambu yang melarangnya pada?\n" +
+"   a. Jalan yang menurun\n" +
+"   b. Jalan yang berlubang-lubang\n" +
+"   c. Jalan tikungan\n" +
+"   Jawaban : C\n" +
+"\n" +
+"17.  Pengemudi dilarang berhenti?\n" +
+"   a. Pada belokan, persimpangan dan jembatan\n" +
+"   b. Tanpa terpaksa pada belokan, persimpangan, dan jembatan, pada jalan-jalan yang \n" +
+"        ada rambu larangan parkir\n" +
+"   c. Tanpa terpaksa, berhenti pada jalan belokan, persimpangan, dan jembatan, \n" +
+"          pada jalan-jalan yang ada rambu larangan berhenti\n" +
+"    Jawaban : C\n" +
+"\n" +
+"18.  Dilarang parkir kendaraan?\n" +
+"    a. Di belokan/ persimpangan jalan milik perkebunan yang tidak ada tanda larangan \n" +
+"        berhenti\n" +
+"    b. Pada jalan yang naik/turun\n" +
+"    c. Dibelokan, persimpangan-persimpangan jalan, jembatan dan tempat lain yang ada \n" +
+"        rambu  larangan parkir\n" +
+"    Jawaban : C\n" +
+"\n" +
+"\n" +
+"             19.  Pada suatu ruas jalan ada 2(dua) macam marka jalan yang berupa tanda \n" +
+"     garis membujur berwarna putih yang satu utuh dan yang disebelahnya lagi \n" +
+"     putus-putus, tanda garis mana yang harus dipatuhi oleh pengemudi\n" +
+"     a. Yang terdekat\n" +
+"     b. Yang terjauh\n" +
+"     c. Kedua-duanya\n" +
+"     Jawaban : A\n" +
+"             \n" +
+"              20. SIM apakah yang perlu dimiliki, bila anda akan mengemudikan sepeda motor \n" +
+"     300 CC dengan kereta samping?\n" +
+"           a. SIM A\n" +
+"     b. SIM C\n" +
+"     c. SIM D\n" +
+"     Jawaban : C\n" +
+"\n" +
+"21.  Anda diwajibkan menggunakan pesawat penunjuk apabila?\n" +
+"       a. Hendak berpapasan\n" +
+"       b. Hendak mundur\n" +
+"       c. Hendak beralih kejalur lain di jalan yang terbagi atas beberapa jalur\n" +
+"       Jawaban : C\n" +
+"\n" +
+"22.  Pengemudi kendaraan bermotor yang terbukti beberapa kali melakukan \n" +
+"     pelanggaran lalu lintas atau pengemudi kendaraan bermotor yang terlibat \n" +
+"     sebagai tersangka dalam kasus kecelakaan lalu lintas dengan korban luka \n" +
+"     berat atau meninggal dunia, maka POLRI  berwenang untuk?\n" +
+"       a. Membatalkan SIM-nya\n" +
+"       b. Melakukan uji ulang\n" +
+"       c. Mencabut SIM-nya\n" +
+"       Jawaban : B\n" +
+"\n" +
+"\n" +
+"\n" +
+"\n" +
+"23.  Apabila petugas mengatur lalu lintas dengan sempritan, tiupan panjang satu kali\n" +
+"      berarti?\n" +
+"        a. Jalan\n" +
+"        b. Berhenti \n" +
+"        c. Meminta perhatian pemakai jalan\n" +
+"        Jawaban : B\n" +
+"24.  Tanda kendaraan bermotor dianggap sah apabila?\n" +
+"        a. Dibuat sendiri oleh pemilik kendaraan bermotor, asalkan sesuai dengan \n" +
+"            persyaratan undang- undang\n" +
+"        b. Dibuat sendiri sembil meunggu pelat nomor asli dari POLRI\n" +
+"        c. Diperoleh dari polisi lalu lintas yang mengeluarkan STNK dan pelat nomor\n" +
+"        Jawaban : C\n" +
+"25.   Apa yang anda harus lakukan bila anda melihat pejalan kaki menunggu untuk \n" +
+"             menyebrang di tempat zebra cross?\n" +
+"         a. Lewati tempat penyebrangan secepat mungkin\n" +
+"         b. Dekati tempat penyebrangan pejalan kaki dengan kecepatan yang sedemikian \n" +
+"             rupa, sehingga bila perlu dapat dihentikan\n" +
+"         c. Jangan berhenti, anda berhak melintas lebih dahulu\n" +
+"         Jawaban : B\n" +
+"26.   Bagaimana seharusnya sikap yang terpuji dari setiap pengemudi?\n" +
+"         a. Memperlihatkan kepandaian dan kelihaian mengemudi kepada pemakai jalan \n" +
+"             lain\n" +
+"         b. Sedemikian rupa sehingga selalu mengalah dan berjalan dipinggir jalan\n" +
+"         c. Sedemikian rupa sehingga keselamatan dan keamanan lalu lintas selalu \n" +
+"             diutamakan\n" +
+"         Jawaban : C\n" +
+"27.  STNK selalu terkait dengan?\n" +
+"       a. Seorang dan kendaraan bermotor\n" +
+"       b. Orang tertentu dan daerah tempat kendaraan itu menetap\n" +
+"       c. Kendaraan bermotor, orang tertentu, dan daerah tempat kendaraan itu menetap\n" +
+"       Jawaban : C\n" +
+"\n" +
+"28.  Helmisasi perlu dilakukan untuk mencegah cidera pada bagian kepala bila \n" +
+"       mengalami suatu kecelakaan, ketentuan yang harus diikuti adalah sebagai \n" +
+"       berikut?\n" +
+"       a. Pengemudi saja yang harus memakai helm\n" +
+"       b. Pengemudi dan pemboceng harus memakai helm\n" +
+"       c. Pengemudi sepeda motor yang kurang dari 100 CC tidak diharuskan memakai \n" +
+"              helm\n" +
+"       Jawaban : B\n" +
+"\n" +
+"29.  Perubahan yang tidak boleh dilakukan pada sepeda motor yang dapat \n" +
+"       menyebabkan STNK tidak sah lagi adalah?\n" +
+"       a. Memasang dan mengganti mesin dengan kemampuan yang lebih besar\n" +
+"       b. Mengganti kaca spion dan tempat duduk\n" +
+"       c. Mengadakan perubahan pada sistem pembuangan (modifikasi klnalpot)\n" +
+"       Jawaban : A\n" +
+"\n" +
+"30.  Kendaraan apa yang berbahaya untuk didahului dan harus didahului hanya pada \n" +
+"       jarak yang memadai untuk mendahului?\n" +
+"        a. Kendaraan roda 2\n" +
+"        b. Kendaraan roda 3\n" +
+"        c. Kendaraan roda 4\n" +
+"        Jawaban : A\n" +
+"\n" +
+"31.  Sebagai pengemudi motor maka?\n" +
+"        a. Untuk mencapai tujuan yang lebih cepat diperbolehkan zig-zag diantara mobil, \n" +
+"            asalkan tidak terjadi kecelakaan\n" +
+"         b. Hanya boleh mendahului dari sebelah kanan apabila tidak ada rintangan lalu \n" +
+"             lintas lain dan dapat dilakukan dengan aman\n" +
+"         c. Diperbolehkan mendahului kendaraan lain dari kiri atau kanan\n" +
+"         Jawaban : B\n" +
+"\n" +
+"32.  Perbuatan pertama apa yang anda lakukan sebagai pengemudi apabila terjadi \n" +
+"       tabrakan?\n" +
+"       a. Segera hentikan kendaraan, biarpun ditengah jalan. Yang penting adalah \n" +
+"           menunjukkan bekas-bekas rem kepada petugas polisi bahwa anda berada\n" +
+"           pada jalur yang benar sesuai  peraturan.\n" +
+"       b. Segera menghentikan kendaraan dan mendatangi pengemudi kendaraan lawan \n" +
+"                  tabrakan untuk meminta pertanggungjawaban kerugian yang anda derita\n" +
+"       c. Segera menghentikan kendaraan dan melaporkan kejadian kecelakaan tersebut \n" +
+"           kepada pos polisi terdekat\n" +
+"        Jawaban : C\n" +
+"\n" +
+"33.   Apabila anda mendengar suara sirine yang kemungkinan dari pemadam pemadam \n" +
+"           kebakaran, polisi atau konvoi kendaraan, anda diharuskan?\n" +
+"        a. Menambah kecepatan\n" +
+"        b. Menepi dan berhenti sampai konvoi lewat dan jalan aman\n" +
+"        c. Menepi dan jalan terus, kalau memungkinkan mengikuti karaoke kendaraan \n" +
+"            yang  mengeluarkan bunyi/suara sirine\n" +
+"        Jawaban : B\n" +
+"      34.  Perubahan pada sepeda motor yang dapat menyebabkan STNK nya tidak sah lagi \n" +
+"       adalah?\n" +
+"       a. Memasang/mengganti mesin dengan kemampuan yang lebih besar\n" +
+"       b. Mengganti kaca spion\n" +
+"       c. Mengadakan perubahan pada sitem pembuangan\n" +
+"       Jawaban : A\n" +
+"\n" +
+"35.  Bagaimana pengemudi sepeda motor dapat mengetahui bahwa lampu besarnya \n" +
+"        menyala dengan sinar jarak jauh?\n" +
+"        a. Dari pelaku pemakai jalan yang brpapasan yang memainkan lampu jauh \n" +
+"             berganti-ganti  dengan lampu pendek\n" +
+"        b. Lampu petunjuk kecil berwarna biru diatas dashboard sepeda motor\n" +
+"        c. Kedua petunjuk diatas benar\n" +
+"        Jawaban : C\n" +
+"\n" +
+"36.  Bila hendak membonceng penumpang , sepeda motor harus dilengkapi dengan?\n" +
+"        a. Tempat duduk, injakan kaki, dan pegangan untuk yang dibelakang\n" +
+"        b. Injakan kaki dan pegangan untuk yang dibonceng\n" +
+"        c. Tempat untuk barang yang dibonceng\n" +
+"        Jawaban : A\n" +
+"\n" +
+"\n" +
+" \n" +
+"37.  Berapa jumlah berat yang diperbolehkan untuk kendaraan bermotor bukan sepeda \n" +
+"      motor yang dapat dikemudikan dengan SIM golongan A ?\n" +
+"      a. Kurang dari 2 Ton\n" +
+"      b. Kurang dari 2,5 Ton\n" +
+"      c. Kurang dari 3,5 Ton\n" +
+"      Jawaban : A\n" +
+"\n" +
+"38.  Lampu apa yang anda nyalakan pada siang hari apabila mengemudikan kendaraan \n" +
+"      tanpa lampu kabut didaerah yang berkabut tebal ?\n" +
+"      a. Lampu DIM (Sinar Jarak Dekat)\n" +
+"      b. Lampu Parkir\n" +
+"      c. Lampu Darurat yang berkedip-kedip\n" +
+"      Jawaban : A \n" +
+"\n" +
+"39.  Seorang pengemudi dibenarkan untuk meninggalkan tempat kejadian \n" +
+"     kecelakaan yang dialaminya, apabila :\n" +
+"     a. Terancam jiwanya dan menyembunyikan diri agar tidak diketahui orang.\n" +
+"     b. Terancam bahaya penganiyayaan oleh Masyarakat yang marah atau emosi, \n" +
+"          tetapi harus segera melaporkan kejadian tersebut ke Pos Polisi terdekat\n" +
+"      c. Melarikan diri dan sembunyi.\n" +
+"      Jawaban : B\n" +
+"\n" +
+"40.  Marka jalan berupa tanda garis membujur yang utuh berwarna kuning pada bahu\n" +
+"      (Bingkai jalan) antara lain berfungsi sebagai :\n" +
+"      a. Tanda batas tempat parkir \n" +
+"      b. Tanda batas paling luar dari sisi kiri jalan lalu lintas\n" +
+"      c. Tanda batas tempat berhenti.\n" +
+"      Jawaban : B\n" +
+"\n" +
+"41.  Bagi Seorang Pengemudi yang terlibat sebagai tersangka dalam Kasus kecelakaan \n" +
+"     Lalu Lintas dengan akibat Korban luka berat atau meninggal dunia, atau \n" +
+"     si-pengemudi terbukti beberapa kali melakukan pelanggaran Lalu Lintas, \n" +
+"     Polri Berwenang untuk:\n" +
+"     a. Membatalkan SIM nya\n" +
+"     b. Mencabut SIM nya\n" +
+"     c. Melakukan uji ulang\n" +
+"     Jawaban : C\n" +
+"\n" +
+"42.  Pencabutan SIM Seseorang oleh hokum berakibat :\n" +
+"      a. Orang tersebut untuk sementara waktu tidak boleh mengemudi sampai \n" +
+"           batas putusan hukum terakhir\n" +
+"      b. Orang tersebut untuk selamanya tidak boleh mengemudi\n" +
+"      c. Orang tersebut dapat mengemudi asal mendapat SIM nya kembali dari Polisi\n" +
+"      Jawaban : A\n" +
+"\n" +
+"43.  Menurut Peraturan Pemerintah Tentang Dana kecelakaan Lalu Lintas \n" +
+"     Korban Korban Kecelakaan yang tidak berhak memperoleh ganti kerugian adalah :\n" +
+"      a. Para korban yang belum mencapai usia dewasa\n" +
+"      b. Para korban yang bunuh diri, atau dalam keadaan mabuk atau \n" +
+"          melakukan kejahatan\n" +
+"      c. Pengemudi yang menjadi korban yang tidak memiliki SIM\n" +
+"      Jawaban : B\n" +
+"\n" +
+"44.  Pada umumnya bila tidak ditentukan lain, kecepatan maksimum yang berlaku\n" +
+"     didalam daerah perumahan atau Kota :\n" +
+"     a. 40 Km/jam\n" +
+"     b. 50 Km/jam\n" +
+"     c. 60 Km/jam\n" +
+"     Jawaban : A\n" +
+"\n" +
+"45.  Apa yang anda lakukan sebelum berpapasan dengan lalu lintas yang datang dari \n" +
+"     depan ?\n" +
+"      a. Waspada terhadap lalu lintas yang datang dari depan dan yang mengikuti dari\n" +
+"           belakang dan mengelak ke kiri.\n" +
+"       b.Tetap pada lajur yang ditempuh\n" +
+"       c. Menggunakan isyarat petunjuk arah sebelah kanan\n" +
+"       Jawaban : A\n" +
+"\n" +
+"46.  Mengurangi kecepatan tinggi dengan mengganti persneling tinggi ke rendahi \n" +
+"       (Engine brake) menghadapi jalan menurun curam :\n" +
+"        a. Tidak perlu karena pemborosan\n" +
+"        b. Perlu sekali\n" +
+"        c. Hanya kalau perlu\n" +
+"        Jawaban : B\n" +
+"\n" +
+"47.  Yang termasuk mobil penumpang antara lain :\n" +
+"        a. Sedan, Station Wagon, dan Bemo.\n" +
+"        b. Sedan, Mini Bus, dan Mobil Jenazah\n" +
+"        c. Bemo, Station Wagon, Bemo, dan Mobil Jenazah.\n" +
+"        Jawaban : A\n" +
+"\n" +
+"\n" +
+"\n" +
+"48.  Apa yang harus anda ketahui tentang keadaan permukaan jalan pada saat turun \n" +
+"       hujan, Khususnya setelah mengalami musim panas (kemarau ) yang panjang ?\n" +
+"       a. Jalan Menjadi basah oleh genangan air.  \n" +
+"       b. Pemandangan jalan akan terganggu. \n" +
+"       c. Aka nada Lapisan licin yang berbahaya diatas permukaan jalan . \n" +
+"       Jawaban : C  \n" +
+"\n" +
+"49.  Anda termasuk katagori pengemudi yang baik, apabila :\n" +
+"        a. Mengendarai mobil dengan hati-hati.\n" +
+"        b. Dapat mengendalikan diri, tidak cepat naik darah atau emosi, mempunyai rasa \n" +
+"             lapang dada dan kepala dingin dalam segala situasi serta mempunyai \n" +
+"             keterampilan mengemudi yang tinggi\n" +
+"         c. Mengendarai mobil dengan keterampilan yang tinggi.\n" +
+"         Jawaban : B \n" +
+"\n" +
+"50.  Anda sedang mengemudikan mobil dengan kecepatan tinggi di jalan bebas \n" +
+"        hambatan atau Tol, untuk berapa lama anda diperbolehkan berada di jalur paling \n" +
+"        kanan?\n" +
+"        a. Saat-saat yang dibutuhkan untuk melewati kendaraan kendaraan lain\n" +
+"        b. Tidak ada larangan untuk bejalan di jalur paling kanan\n" +
+"        c. Sepanjang anda masih mengikuti kendaraan lain yang berada di lajur kanan \n" +
+"            yang belum menyelesaikan gerakan mendahului\n" +
+"        Jawaban : A");
+    }
+    
+}
