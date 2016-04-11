43c5919fdd3094072b1ebe678d46b8fc4def4172
adelfadli <adelfadly02@gmail.com>
2016-04-11 23:59:28 +0700

    versi 3.2.2

---
+++ build/classes/javaapplication3/JavaApplication3.rs
@@ -0,0 +1,1 @@
+fandp.fandp
---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,29 @@
+package javaapplication3;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+  System.out.println("Assalamualaikum");
+  System.out.println("Kaum muda");
+  System.out.println("Kun anta");
+  System.out.println("Love teu eureun-eureun");
+  System.out.println("Assalamualaikum");
+  System.out.println("5+5");
+  System.out.println("Itu adalah hasilnya");
+    String nm = "Adel";
+    String nm2 = "Fadli";
+    String nm3 = "Adalah";
+    String nm4 = "seorang";
+    String nm5 = "Mahasiswa";
+    
+    System.out.println(nm+" "+nm2+" "+nm3+" "+nm4+" "+nm5);
+      System.out.println(6+2);
+      System.out.println(6-2);
+       System.out.println(6*2);
+        System.out.println(6/2);
+       
+       
+                
+         }
+    }
+  
